f902f93460932b8ecbf890cb72e0e3ec706de12f
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2025-09-25 07:13:44 +0300

    Write the pseudocode in script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+// Program will prompt the user for a choice and writes the computers random choice
+//  and adds a point to the winner and give the score for the players and the round number for 5 rounds 
+//  then declares the winner in the game
+
+// Declare a variable for the user choice.
+// Declare a variable for the random computer choice.
+// Initialize a variable for the user score with 0.
+// Initialize a variable for the computer score with 0.
+// Compare the two choices and declare the winner.
+// Add 1 to the winners score.
+// Repeat five times then declare the winner in the game
+
+// Prompt the user for a choice and assign it to the user var.
+
+// Take a random choice and assign it to the computer var,
+//   and print it in the console.
+
+// If one chooses rock it will win to scissors and loses to paper
+// If one chooses paper it will win to rock and loses to paper
+// If one chooses scissors it will win to paper and loses to rock 
+//  then add 1 to the winners score var.
